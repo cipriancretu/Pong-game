769f74be1e49b307832f75e4edf0e8668aeafa47
Ciprian Cretu <124049021+cipriancretu@users.noreply.github.com>
2023-05-12 18:09:04 +0100

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -1,8 +1,9 @@
+"""Import the turtle library"""
 from turtle import Turtle
 
 
 class Paddle(Turtle):
-    
+    """This function set up the paddles para
     def __init__(self, position):
         super().__init__()
         self.shape("square")
@@ -10,11 +11,11 @@ class Paddle(Turtle):
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
-
+    """This function set the moving of the paddle up"""
     def go_up(self):
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
-
+    """This function set the moving of the paddle down"""
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
